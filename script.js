007dcb0409d0d31eb84b3a7c45a54d00dd117228
GUchechi <godswilluchendu7@gmail.com>
2022-09-02 22:44:26 +0100

    added class constructors

--- script.js
+++ script.js
@@ -1,27 +1,11 @@
-var userOne = {
-    email: 'ryu@ninjas.com',
-    name: 'Ryu',
-    login(){
-        console.log(this.email, 'has logged in');
-    },
-    logout(){
-        console.log(this.email, 'has logged out');
+class User {
+    constructor(email, name){
+        this.email = email;
+        this.name = name;
     }
-};
+}
 
-var prop = 'name'
+let userOne = new User("uche@gmail.com", "Uche");
+let userTwo = new User("godswill@gmail.com", "Godswill");
 
-// userOne.email = 'ryu2k18@ninjas.com';
-// userOne['email'] = 'ryu2k18@ninjas.com';
-// userOne[prop] = 'Ryuke';
-
-// console.log(userOne);
-
-userOne.age = 25;
-userOne.logInfo = function(){
-    console.log(this.name, this.age, this.email);
-};
-
-userOne.logInfo();
-userOne.login();
-userOne.logout();
+console.log(userOne, userTwo);
