a76576605c569a816e3ba9f0cf370c557350c7d7
GUchechi <godswilluchendu7@gmail.com>
2022-09-02 16:42:33 +0100

    updating properties

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-let userOne = {
-    email : "uche@gmail.com",
-    name : "Uche", 
+var userOne = {
+    email: 'ryu@ninjas.com',
+    name: 'Ryu',
     login(){
-        console.log(userOne.email, 'has logged in');
+        console.log(this.email, 'has logged in');
     },
-    logOut(){
-        console.log(this.email, 'logged out');
-    } 
+    logout(){
+        console.log(this.email, 'has logged out');
+    }
 };
 
-console.log(userOne.name);
-console.log(userOne.email);
-console.log(userOne.login());
-console.log(userOne.logOut());
+var prop = 'name'
+
+// userOne.email = 'ryu2k18@ninjas.com';
+// userOne['email'] = 'ryu2k18@ninjas.com';
+// userOne[prop] = 'Ryuke';
+
+// console.log(userOne);
+
+userOne.age = 25;
+userOne.logInfo = function(){
+    console.log(this.name, this.age, this.email);
+};
+
+userOne.logInfo();
+userOne.login();
+userOne.logout();
