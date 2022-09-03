6d9c34c9f85fac5fbd386d4dca2d8c58f3dac3c6
GUchechi <godswilluchendu7@gmail.com>
2022-09-03 21:57:36 +0100

    added class User

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
 class User {
-    constructor(email, name){
-        this.email = email;
-        this.name = name;
-    }
+    
 }
-
-let userOne = new User("uche@gmail.com", "Uche");
-let userTwo = new User("godswill@gmail.com", "Godswill");
-
-console.log(userOne, userTwo);
