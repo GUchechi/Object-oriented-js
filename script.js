ed5e111d12d132bcee2dea9a620e671cf8141b3c
GUchechi <godswilluchendu7@gmail.com>
2022-09-03 22:03:09 +0100

    added methods

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 class User {
-    constructor() {
+    constructor(email, name) {
         this.email = email;
         this.name = name;
     }
+    login(){
+        console.log(this.email, "just logged in");
+    }
+    logout(){
+        console.log(this.name, "just logged out");
+    }
 }
 
-let userOne = new User();
-let userTwo = new User();
+let userOne = new User("uche@gmail.com", "Uche");
+let userTwo = new User("godswill@gmail.com", "Godswill");
+
+console.log(userOne.name)
+console.log(userTwo)
+
+userOne.login()
+userTwo.logout()
