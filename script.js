9d7569ef5d86fc0646c6eb9469a5d0c0796e618f
GUchechi <godswilluchendu7@gmail.com>
2022-09-03 22:08:08 +0100

    added instance

--- script.js
+++ script.js
@@ -2,20 +2,25 @@ class User {
     constructor(email, name) {
         this.email = email;
         this.name = name;
+        this.age = 0;
     }
     login(){
         console.log(this.email, "just logged in");
+        this;
     }
     logout(){
         console.log(this.name, "just logged out");
+        this;
+    }
+    updateAge(){
+        this.age ++;
+        console.log(this.name, "is now" , this.age, "old" );
+        this;
     }
 }
 
 let userOne = new User("uche@gmail.com", "Uche");
 let userTwo = new User("godswill@gmail.com", "Godswill");
 
-console.log(userOne.name)
-console.log(userTwo)
 
-userOne.login()
-userTwo.logout()
+console.log(userOne.updateAge())
