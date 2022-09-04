5b8501a0cc930f37b2c9661aa3ad5ce3e944023e
GUchechi <godswilluchendu7@gmail.com>
2022-09-04 19:35:11 +0100

    added class inheritance

--- script.js
+++ script.js
@@ -2,25 +2,40 @@ class User {
     constructor(email, name) {
         this.email = email;
         this.name = name;
-        this.age = 0;
+        this.score = 0;
     }
     login(){
         console.log(this.email, "just logged in");
-        this;
+        return this;
     }
-    logout(){
-        console.log(this.name, "just logged out");
-        this;
+    logOut(){
+        console.log(this.email, "just logged out");
+        return this;
     }
-    updateAge(){
-        this.age ++;
-        console.log(this.name, "is now" , this.age, "old" );
-        this;
+    updateScore(){
+        this.score++;
+        console.log(this.email, "score updated to", this.score);
+        return this;
     }
 }
 
-let userOne = new User("uche@gmail.com", "Uche");
-let userTwo = new User("godswill@gmail.com", "Godswill");
+class Admin extends User {
+    deleteUser(user) {
+        users = users.filter(u => {
+            return u.email !== user.email;
+        })
+    }
+}
 
 
-console.log(userOne.updateAge())
+const userOne = new User( "godswill@gmail.com", "Godswill");
+const userTwo = new User ("uche@gmail.com", "Uche")
+const admin = new Admin ( "admin@gmail.com", "admin");
+
+let users = [userOne, userTwo, admin];
+
+admin.deleteUser(userTwo);
+
+console.log(users)
+
+
