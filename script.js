15770efeaf8ef116fba0b64ee28780ce736c272b
GUchechi <godswilluchendu7@gmail.com>
2022-09-04 19:53:10 +0100

    constructor under hood

--- script.js
+++ script.js
@@ -1,41 +1,17 @@
-class User {
-    constructor(email, name) {
-        this.email = email;
-        this.name = name;
-        this.score = 0;
-    }
-    login(){
-        console.log(this.email, "just logged in");
-        return this;
-    }
-    logOut(){
-        console.log(this.email, "just logged out");
-        return this;
-    }
-    updateScore(){
-        this.score++;
-        console.log(this.email, "score updated to", this.score);
-        return this;
-    }
-}
-
-class Admin extends User {
-    deleteUser(user) {
-        users = users.filter(u => {
-            return u.email !== user.email;
-        })
+function User (email, name){
+    this.email = email;
+    this.name = name;
+    this.online = false;
+    this.login = function(){
+        console.log(this.name, "has logged in");
     }
 }
 
 
 const userOne = new User( "godswill@gmail.com", "Godswill");
 const userTwo = new User ("uche@gmail.com", "Uche")
-const admin = new Admin ( "admin@gmail.com", "admin");
 
-let users = [userOne, userTwo, admin];
-
-admin.deleteUser(userTwo);
-
-console.log(users)
+console.log(userOne)
+userTwo.login();
 
 
