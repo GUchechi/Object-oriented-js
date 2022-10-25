4761a9de6e8f3487c0d94271cc2ad5c7f1e55186
GUchechi <godswilluchendu7@gmail.com>
2022-10-25 16:42:14 +0100

    added prototype

--- Lessons/lesson2.js
+++ Lessons/lesson2.js
@@ -37,3 +37,43 @@
 // console.log(users)
 
 //  Prototype
+function User(email, name) {
+    this.email = email;
+    this.name = name;
+    this.online = true;
+}
+
+User.prototype.login = function(){
+    this.online = false;
+    console.log(this.name, 'has logged in');
+}
+
+
+User.prototype.logout = function(){
+    this.online = true;
+    console.log(this.name, 'has logged out');
+}
+
+function Admin (...args) {
+    User.apply(this,args)
+    this.role='super admin';
+}
+
+Admin.prototype = Object.create(User.prototype)
+
+// Add another method to Admin
+Admin.prototype.deleteUser = function (u){
+     users = users.filter(user => {
+        return u.email != user.email
+     });
+}
+
+let userOne = new User ('godswilluchendu7@gmail.com', 'Godswill');
+let userTwo = new User ('uchechi@gmail.com', 'Uchechi');
+let admin = new Admin('strings@gmail.com', 'String')
+let users = [userOne, userTwo, admin];
+
+console.log(admin.login())
+
+admin.deleteUser(userOne)
+console.log(users)
